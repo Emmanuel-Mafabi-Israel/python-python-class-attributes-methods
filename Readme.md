d842a655ae3d406664cf01cf96d87588759624dc
Emmanuel-Mafabi-Israel <mafabi_emmanuel@outlook.com>
2024-12-03 15:37:56 +0300

    mafabi: project creation and initialization

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# Python Class Attributes and Methods - Israel Mafabi Emmanuel
+
+This is a simple Python application that showcases the usage of Python Class Attributes and Methods
+
+More so how to interact with these... 😉
+
+The Two Scopes:
+
+- Class **global** scope
+- Instance scope
+
+## Requirements
+
+- Python 3.13 or above
+- `importlib-metadata` version 8.5.0
+- `importlib-resources` version 6.4.5
+- `pytest` version 8.3.3
+
+## Installation
+
+1. Clone the repository:
+    ```sh
+    git clone <repository-url>
+    ```
+
+2. Navigate to the project directory:
+    ```sh
+    cd <repository-directory>
+    ```
+
+3. Install the dependencies using Pipenv:
+    ```sh
+    pipenv install
+    ```
+
+## Usage
+
+To run tests on the file, execute the following command:
+```sh
+pytest # should be in the parent root directory!!! or inside lib/testing
+```
---
+++ lib/song.py
@@ -0,0 +1,71 @@
+# GLORY BE TO GOD,
+# PYTHON PROGRAMMING - PYTHON - CLASS ATTRIBUTES AND METHODS
+# BY ISRAEL MAFABI EMMANUEL
+
+class Song:
+    # Class scope - Global scope
+    count:int = 0
+    genre_count:dict  = {}
+    artist_count:dict = {}
+
+    genres:list  = []
+    artists:list = []
+
+    def __init__(self, name:str, artist:str, genre:str):
+        self.name   = name
+        self.artist = artist
+        self.genre  = genre
+
+        Song.add_song_to_count()
+        Song.add_to_artists(artist)
+        Song.add_song_to_genre(genre)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_artists(cls, _artist_):
+        if _artist_ not in cls.artists:
+            # we add to the list
+            cls.artists.append(_artist_)
+        else:
+            print("Artist already recorded...")
+
+    @classmethod
+    def add_song_to_genre(cls, _genre_):
+        if _genre_ not in cls.genres:
+            # we add to the list
+            cls.genres.append(_genre_)
+        else:
+            print("Song is alread in the Genre!")
+
+    @classmethod
+    def add_to_genre_count(cls, _genre_):
+        # song is present in the genre list
+        if _genre_ in cls.genre_count:
+            # we increment by one...
+            cls.genre_count[_genre_] += 1
+        else:
+            cls.genre_count[_genre_] = 1
+
+
+    @classmethod
+    def add_to_artist_count(cls, _artist_):
+        # song is present in the genre list
+        if _artist_ in cls.artist_count:
+            # we increment by one...
+            cls.artist_count[_artist_] += 1
+        else:
+            cls.artist_count[_artist_] = 1
+
+    @classmethod
+    def print_songs(cls):
+        print(f"Genre Count: {cls.genre_count}")
+        print(f"Genre List : {cls.genres}")
+        print(f"Artist List: {cls.artists}")
+
+song_1 = Song("This is the Life", "Amy", "Rock")
+Song.print_songs()
